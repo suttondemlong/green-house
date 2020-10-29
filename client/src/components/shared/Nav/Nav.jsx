8a64e50a47395d9e319f4f611bf76611362d55a2
Dean <dean.g.leong@gmail.com>
2020-10-29 19:22:04 -0400

    updated logo to a p so that font size can be applied. Need icon for login

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -4,13 +4,15 @@ import '../Nav.css'
 function Nav(props) {
   return (
     <div className="nav-bar">
-      <h1>GreenHouse</h1>
-      <p>Explore Green Tech</p>
-      <p>Dashboard</p>
-      <p>Meet our Experts</p>
+      <p className="app-name">GreenHouse</p>
+      <p className="links">Explore Green Tech</p>
+      <p className="links">Dashboard</p>
+      <p className="links">Meet our Experts</p>
       <input className="nav-search-bar" type="text" placeholder="Search"></input>
     </div>
   );
 }
 
 export default Nav;
+
+//login logo?
