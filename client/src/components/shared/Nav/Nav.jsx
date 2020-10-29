3035f0d05aed6da5b36301e3db95e4a698b0e122
Dean <dean.g.leong@gmail.com>
2020-10-29 18:17:44 -0400

    basic header setup: text, search input

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../Nav.css'
 
 function Nav(props) {
   return (
-    <div>
-      This is the Nav Bar
+    <div className="nav-bar">
+      <h1>GreenHouse</h1>
+      <p>Explore Green Tech</p>
+      <p>Dashboard</p>
+      <p>Meet our Experts</p>
+      <input className="nav-search-bar" type="text" placeholder="Search"></input>
     </div>
   );
 }
