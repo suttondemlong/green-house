e9be61e22522bab2c67e997189bb6d385b430184
Sutton <suttondemlong@gmail.com>
2020-10-30 15:59:12 -0400

    Establish Layout Component

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -3,22 +3,21 @@ import Buying from '../../components/Buying/Buying'
 import ChatHomePage from '../../components/ChatHomePage/ChatHomePage'
 import Directions from '../../components/Directions/Directions';
 import HomeImage from "../../components/HomeImage/HomeImage"
-import Nav from "../../components/shared/Nav/Nav"
-import Footer from "../../components/shared/Footer/Footer"
+import Layout from '../../components/shared/Layout/Layout'
 import './Home.css';
 
 function Home(props) {
   return (
-    <div>
-      <Nav />
-      <div className="content-container">
-        <HomeImage />
-        <Directions />
-        <ChatHomePage />
-        <Buying />
+    <Layout>
+      <div>
+        <div className="content-container">
+          <HomeImage />
+          <Directions />
+          <ChatHomePage />
+          <Buying />
+        </div>
       </div>
-      <Footer />
-    </div>
+    </Layout>
   );
 }
 
