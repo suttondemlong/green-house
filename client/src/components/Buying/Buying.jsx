69f9fbab172638a88a47e80510422c8a3cfdd6ab
Amber Moroe <ambermoorexc@gmail.com>
2020-10-30 10:28:50 -0500

    Styling and classNames for buying component

--- client/src/components/Buying/Buying.jsx
+++ client/src/components/Buying/Buying.jsx
@@ -5,9 +5,9 @@ function Buying(props) {
     <div>
       <h2>What people are buying</h2>
       <div className='items'>
-        <img src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
-        <img src='green-house/client/src/assets/plug1.jpg'alt='An eco-friendly plug'/>
-        <img src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
+        <img className='image' src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
+        <img className='image' src='green-house/client/src/assets/plug1.jpg'alt='An eco-friendly plug'/>
+        <img className='image' src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
       </div>
     </div>
   );
