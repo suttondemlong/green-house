0f6deeac18d1b70aef59159930310fbcd23fdd9b
Amber Moroe <ambermoorexc@gmail.com>
2020-10-30 10:05:02 -0500

    Structure of static items

--- client/src/components/Buying/Buying.jsx
+++ client/src/components/Buying/Buying.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 function Buying(props) {
   return (
     <div>
-      BUYING Hello World
+      <h2>What people are buying</h2>
+      <div className='items'>
+        <img src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
+        <img src='green-house/client/src/assets/plug1.jpg'alt='An eco-friendly plug'/>
+        <img src='green-house/client/src/assets/light-bulb1.jpeg' alt='An Edison light bulb'/>
+      </div>
     </div>
   );
 }
