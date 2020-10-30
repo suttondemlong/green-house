3194d62ce4767de2cd5a4c76e7357d61aa38ce09
Amber Moroe <ambermoorexc@gmail.com>
2020-10-29 20:58:30 -0500

    Options was styled and is nested in Directions which is also styled

--- client/src/components/Directions/Directions.jsx
+++ client/src/components/Directions/Directions.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import '../Directions/Directions.css'
 import Options from "../Options/Options.jsx"
 
 function Directions(props) {
   return (
-    <div>
-      Directions
-      <Options />
-      {/* Pass props for options */}
-      |
-      <Options />
-      |
-      <Options />
-    </div>
+    <div className='directions-container'>
+      
+      <Options title='Discover new eco-tech' p='Get information on the newest and most popular eco-friendly brands'/>
+      <div className='vertical-line'></div>
+      <Options title='Track your savings' p='Get quantifiable data on your eco-friendly technology'/>
+      <div className='vertical-line'></div> 
+      <Options id='tech' title='View your technology' p='All your electronic items are viewable in one platform'/>
+      </div>
   );
 }
 
