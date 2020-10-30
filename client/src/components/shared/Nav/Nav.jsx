657fb0ed15c0791b5f0d5000f405afa1bd9c4960
Dean <dean.g.leong@gmail.com>
2020-10-30 11:09:05 -0400

    added icons

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './Nav.css'
+import './Nav.css';
+import login from '../../../../src/assets/login.png';
+import glass from '../../../../src/assets/loupe.png'
 
 function Nav(props) {
   return (
@@ -8,7 +10,8 @@ function Nav(props) {
       <p className="links">Explore Green Tech</p>
       <p className="links">Dashboard</p>
       <p className="links">Meet our Experts</p>
-      <input className="nav-search-bar" type="text" placeholder="Search"></input>
+      <input className="nav-search-bar" type="text" src={glass} placeholder="Search"></input>
+      <img className ="login-icon" src={login} />
     </div>
   );
 }
