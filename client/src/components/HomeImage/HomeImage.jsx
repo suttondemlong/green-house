6cc269112c4a85d7bdbfb24892beb3e4c8a8acd0
Sutton <suttondemlong@gmail.com>
2020-10-29 20:05:48 -0400

    Populate HomeImage with text and background image

--- client/src/components/HomeImage/HomeImage.jsx
+++ client/src/components/HomeImage/HomeImage.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './HomeImage.css';
 
 function HomeImage(props) {
   return (
     <div>
-      HOMEIMAGE PUT AN IMAGE HERE
+      <div className="text-in-image">
+        {/* <img src="home-image.jpg"></img> */}
+        <div className="text-in-image">
+          <div>
+            Join an eco-friendly community.
+          </div>
+          <div>
+            Reach your goals of sustainability.
+          </div>
+          <div>
+            GreenHouse will give you the tools and information you need.
+          </div>
+        </div>
+        <div className="title-gren-hous">
+          GreenHouse /ˈɡrēnˌhous/
+        </div>
+        <div className="definition">
+          De-mystifying green tech in the home for the eco-curious 
+        </div>
+      </div>
     </div>
   );
 }
