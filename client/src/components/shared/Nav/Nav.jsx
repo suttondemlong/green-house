b4be87aca26fa1df14dedfe6587712c9a20a2449
Geoff <geoffreylaporte@gmail.com>
2020-11-02 09:58:39 -0600

    Home Routing from the Title Complete

--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 import './Nav.css';
 import login from '../../../../src/assets/login3.png';
 
@@ -6,7 +7,9 @@ import login from '../../../../src/assets/login3.png';
 function Nav(props) {
   return (
     <div className="nav-bar">
+      <Link to='/' style={{textDecoration: 'none', color: '#ffffff'}}>
       <p className="app-name">GreenHouse</p>
+      </Link>
       <p className="links">Explore Green Tech</p>
       <p className="links">Dashboard</p>
       <p className="links">Meet our Experts</p>
