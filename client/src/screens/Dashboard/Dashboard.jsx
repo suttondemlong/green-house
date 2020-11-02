75bb85ae66d7e48d4351389f6a1fdf50a74cef49
Geoff <geoffreylaporte@gmail.com>
2020-11-02 17:29:19 -0600

    Added the dashboard link and routing from the homepage

---
+++ client/src/screens/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from '../../components/shared/Layout/Layout'
+
+function Dashboard(props) {
+  return (
+    <Layout>
+      <div>
+      <p>This is the dashboard</p>
+      </div>
+    </Layout>
+  );
+}
+
+export default Dashboard;
