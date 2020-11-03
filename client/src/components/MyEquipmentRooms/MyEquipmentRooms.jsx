e2b56a30bafcf9b9643390e6419a12059a8914c4
Dean <dean.g.leong@gmail.com>
2020-11-03 15:12:29 -0500

    idk

--- client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
+++ client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
@@ -9,8 +9,8 @@ function MyEquipmentRooms(props) {
       <div className="my-equip-rooms">
         <img className="equip-rooms-img" src={livingroom} alt='livingroom' />
           <div className="room-info">
-            <h5 className="room-title">Living Room</h5>
-            <h6 className="room-stats">Room Statistics</h6>
+            <p className="room-title">Living Room</p>
+            <p className="room-stats">Room Statistics</p> 
             <p className="room-items">Total Items: 9</p>
           </div>
       </div>
