b464ac703ebf024ff26233d9a4536ea48204f0f9
Geoff <geoffreylaporte@gmail.com>
2020-11-03 10:52:57 -0600

    css updated for wishlist and reviews

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import './Reviews.css'
 import bulb from "../../assets/bulb.jpeg"
 import power from "../../assets/plug.jpg"
 import nest from "../../assets/nest.jpg"
+import add from "../../assets/plus.png"
 
 function Reviews(props) {
   return (
     <>
     <hr className="chat-hr"></hr>
-    <div className='buying'>
+    <div className='reviews'>
       <h2>My Reviews</h2>
-      <div className='items'>
-        <img className='image' src={bulb} alt='A light bulb'/>
-        <img className='image' src={power} alt='Power strip'/>
-        <img className='image' src={nest} alt='Nest thermostat' />
+      <div className='review-items'>
+        <img className='review-image-bulb' src={bulb} alt='A light bulb'/>
+        <img className='review-image-power' src={power} alt='Power strip'/>
+          <img className='review-image-nest' src={nest} alt='Nest thermostat' />
+          <img className='review-image-add' src={add} alt='Add a Review' />
       </div>
       <button className="seemore-button" disabled>
           See All
--- client/src/components/Wishlist/Wishlist.jsx
+++ client/src/components/Wishlist/Wishlist.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import './Wishlist.css'
 import barrel from "../../assets/barrel.jpg"
 import power from "../../assets/plug.jpg"
 import nest from "../../assets/nest.jpg"
+import add from "../../assets/plus.png"
 
 function Wishlist(props) {
   return (
     <>
     <hr className="chat-hr"></hr>
-    <div className='buying'>
+    <div className='wishlist'>
       <h2>My Wishlist</h2>
-      <div className='items'>
-        <img className='image' src={barrel} alt='A rain barrel'/>
-        <img className='image' src={power} alt='Power strip'/>
-        <img className='image' src={nest} alt='Nest thermostat' />
+      <div className='wishlist-items'>
+        <img className='wishlist-image-barrel' src={barrel} alt='A rain barrel'/>
+        <img className='wishlist-image-power' src={power} alt='Power strip'/>
+          <img className='wishlist-image-nest' src={nest} alt='Nest thermostat' />
+          <img className='wishlist-image-add' src={add} alt='My Wishlist' />
       </div>
       <button className="seemore-button" disabled>
           See All
