42a491b5216acecce9bfd4cd16faff9494337f29
Dean <dean.g.leong@gmail.com>
2020-11-03 14:23:59 -0500

    styling

--- client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
+++ client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
@@ -4,12 +4,16 @@ import "./MyEquipmentRooms.css"
 
 function MyEquipmentRooms(props) {
   return (
-    <div className="my-equip-rooms">
-      <hr className="chat-hr"></hr>
-      <img className="equip-rooms-img" src={livingroom} alt='livingroom' />
-      <h4 className="room-title">Living Room</h4>
-      <h3 className="room-stats">Room Statistics</h3>
-      <p className="room-items">Total Items: 9</p>
+    <div>
+       <hr className="chat-hr"></hr>
+      <div className="my-equip-rooms">
+        <img className="equip-rooms-img" src={livingroom} alt='livingroom' />
+          <div className="room-info">
+            <h5 className="room-title">Living Room</h5>
+            <h6 className="room-stats">Room Statistics</h6>
+            <p className="room-items">Total Items: 9</p>
+          </div>
+      </div>
     </div>
   );
 }
