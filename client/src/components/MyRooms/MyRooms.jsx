2b5409739ec7f8bd280e029450368326387dba56
Dean <dean.g.leong@gmail.com>
2020-11-03 11:35:43 -0500

    myrooms folder

---
+++ client/src/components/MyRooms/MyRooms.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyRooms(props) {
+  return (
+    <div>
+      <hr className="chat-hr"></hr>
+      <div className="MyRooms">
+        <h2>My Rooms</h2>
+        <button className="seemore-button">
+          See All
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default MyRooms;
