c4253100e52e1769dfbd0375294e81f0250bceca
Dean <dean.g.leong@gmail.com>
2020-11-03 12:13:19 -0500

    added images

--- client/src/components/MyRooms/MyRooms.jsx
+++ client/src/components/MyRooms/MyRooms.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./MyRooms.css"
+import bathroom from "../../assets/bathroom.jpg"
+import kitchen from "../../assets/kitchen.jpeg"
+import livingroom from "../../assets/livingroom.jpg"
 
 function MyRooms(props) {
   return (
@@ -7,6 +10,11 @@ function MyRooms(props) {
       <hr className="chat-hr"></hr>
       <div className="my-rooms">
         <h2>My Rooms</h2>
+        <div className='rooms'>
+        <img className='myroom-image' src={bathroom} alt='bathroom'/>
+        <img className='myroom-image' src={kitchen} alt='kitchen'/>
+        <img className='myroom-image' src={livingroom} alt='livingroom' />
+        </div>
         <button className="seemore-button">
           See All
         </button>
