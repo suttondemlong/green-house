0cdd188eedb047c76b2cb41b3bd41aaf2d75ef16
Dean <dean.g.leong@gmail.com>
2020-11-04 15:01:28 -0500

    images for the screen

--- client/src/components/SimilarItemsProducts/SimilarItemsProducts.jsx
+++ client/src/components/SimilarItemsProducts/SimilarItemsProducts.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import yellowbulb from "../../assets/yellowbulb.jpeg"
+import whitebulb from "../../assets/whitebulb.jpeg"
+import clearbulb from "../../assets/clearbulb.jpeg"
 
 function SimilarItemsProducts(props) {
   return (
     <div className='buying'>
       <h2>Similar Items</h2>
       <div className="items-similar">
-        <img></img>
-        <img></img>
-        <img></img>
+        <img className='image' src={clearbulb}></img>
+        <img className='image' src={whitebulb}></img>
+        <img className='image' src={yellowbulb}></img>
       </div>
+      <hr className="general-hr"></hr>
+      <h2>Product Comparison</h2>
     </div>
   );
 }
