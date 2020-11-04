0a6871ab21e18a9cff5dc865f31ca30eb24dedaa
Amber Moroe <ambermoorexc@gmail.com>
2020-11-04 11:26:30 -0600

    Finished rough draft structure and styling

--- client/src/screens/EquipmentDetail/EquipmentDetail.jsx
+++ client/src/screens/EquipmentDetail/EquipmentDetail.jsx
@@ -23,10 +23,26 @@ function EquipmentDetail(props) {
 
   return (
     <div>
-      <h2>Equipment Details</h2>
-      <img src={detail.imgURL} alt={detail.name} />
-      <p>{detail.name}</p>
+      <h2 className="e-detail-title">Equipment Details</h2>
+      <img className="e-detail-image" src={detail.imgURL} alt={detail.name} />
+      <p className="e.detail-name">{detail.name}</p>
+      <button className="basic-button">Details</button>
       <hr className='general-hr'></hr>
+      <h2 className="e-detail-title">This Item Saved You</h2>
+      <div className="e-detail-saved-container">
+        <img className="e-detail-image" src={energy} />
+        <div className="e-detail-stats-container">
+          <h5>Total Money Saved</h5>
+          <p>{detail.money}</p>
+          <h5>Total Energy Saved</h5>
+          <p>{detail.energy}</p>
+          <h5>This is equivalent to saving</h5>
+          <p>{detail.trees}</p>
+        </div>
+        <button className="basic-button">
+          Browse Similar Products
+        </button>
+      </div>
     </div>
   );
 }
