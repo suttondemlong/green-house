39b16930e3785d11b7c0ef08fd897eca5a2e6d09
Dean <dean.g.leong@gmail.com>
2020-11-04 16:39:26 -0500

    added checks

--- client/src/components/SimilarItemsProducts/SimilarItemsProducts.jsx
+++ client/src/components/SimilarItemsProducts/SimilarItemsProducts.jsx
@@ -3,6 +3,8 @@ import "./SimilarItemsProducts.css"
 import yellowbulb from "../../assets/yellowbulb.jpeg"
 import whitebulb from "../../assets/whitebulb.jpeg"
 import clearbulb from "../../assets/clearbulb.jpeg"
+import greencheck from "../../assets/green-check.png"
+import greycheck from "../../assets/grey-check.png"
 
 function SimilarItemsProducts(props) {
   return (
@@ -16,20 +18,32 @@ function SimilarItemsProducts(props) {
       <hr className="general-hr"></hr>
       <h2>Product Comparison</h2>
       <div className="comparison">
-        <img className='image' src={whitebulb}></img>
+        <img className='compare-image' src={whitebulb}></img>
           <p className="comparison-detail">Indoor/Outdoor Useage: Indoor Only</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Lasts: 26 Weeks</p>
+            <img className='checks' src={greycheck}></img>
           <p className="comparison-detail">Price: $4.15</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Annual Energy Cost: $4.15</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Watts: 8.5 W</p>
+            <img className='checks' src={greycheck}></img>
           <p className="comparison-detail">Dimmable: No</p>
-        <img className='image' src={clearbulb}></img>
+            <img className='checks' src={greycheck}></img>
+        <img className='compare-image' src={clearbulb}></img>
           <p className="comparison-detail">Indoor/Outdoor Useage: Indoor/Outdoor</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Lasts: 23 Weeks</p>
+            <img className='checks' src={greycheck}></img>
           <p className="comparison-detail">Price: $3.93</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Annual Energy Cost: $1.50</p>
+            <img className='checks' src={greencheck}></img>
           <p className="comparison-detail">Watts: 5 W</p>
+            <img className='checks' src={greycheck}></img>
           <p className="comparison-detail">Dimmable: No</p>
+            <img className='checks' src={greycheck}></img>
       </div>
     </div>
   );
