30156a8ce77a0e5cc50e691314177dc41215ff61
Geoff <geoffreylaporte@gmail.com>
2020-11-04 14:28:13 -0600

    Added Search Functionality to the Add Equipment Page

--- client/src/screens/Equipments/Equipments.jsx
+++ client/src/screens/Equipments/Equipments.jsx
@@ -3,18 +3,29 @@ import './Equipments.css'
 import Layout from '../../components/shared/Layout/Layout'
 import Equipment from "../../components/Equipment/Equipment"
 import { getEquipments } from "../../services/equipments";
+import Search from '../../components/Search/Search';
 
 function Equipments(props) {
   const [equipments, setEquipments] = useState([])
+  const [queriedEquipments, SetQueriedEquipments] = useState([])
 
   useEffect(() => {
     const fetchEquipments = async () => {
       const equipments = await getEquipments()
       setEquipments(equipments)
+      SetQueriedEquipments(equipments)
     }
    fetchEquipments()
   }, [])
-  const equipmentsJSX = equipments.map((equipment, index) => (
+
+  const handleSearch = event => {
+    const searchedEquipments = equipments.filter(equipments => equipments.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    SetQueriedEquipments(searchedEquipments)
+  }
+
+  const handleSubmit = event => event.preventDefault()
+
+  const equipmentsJSX = queriedEquipments.map((equipment, index) => (
     <Equipment
       _id={equipment._id}
       imgURL={equipment.imgURL}
@@ -25,7 +36,8 @@ function Equipments(props) {
   return (
     <Layout>
       <div className="equipment-container">
-      <h2 className="equipments-user">Add Equipment</h2>
+        <h2 className="equipments-user">Add Equipment</h2>
+        <Search onSubmit={handleSubmit} onChange={handleSearch}/>
     <div className="equipments">{equipmentsJSX}</div>
     </div>
     </Layout>
