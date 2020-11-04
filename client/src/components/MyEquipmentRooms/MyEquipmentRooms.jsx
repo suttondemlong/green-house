07997225cec89deee4f07c74c01742e9d17f59d5
Dean <dean.g.leong@gmail.com>
2020-11-04 10:32:00 -0500

    renamed equipment to myequipment

--- client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
+++ client/src/components/MyEquipmentRooms/MyEquipmentRooms.jsx
@@ -7,7 +7,7 @@ function MyEquipmentRooms(props) {
     <div>
        <hr className="chat-hr"></hr>
       <div className="my-equip-rooms">
-        <img className="equip-rooms-img" src={livingroom} alt='livingroom' />
+        <img className="my-equip-rooms-img" src={livingroom} alt='livingroom' />
           <div className="room-info">
             <p className="room-title">Living Room</p>
             <p className="room-stats">Room Statistics</p> 
