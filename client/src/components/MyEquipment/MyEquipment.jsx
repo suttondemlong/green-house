f629cef795f35210f38d625705a1c0a0c91eeaf2
Dean <dean.g.leong@gmail.com>
2020-11-04 10:32:31 -0500

    renamed equipment to myequipment

---
+++ client/src/components/MyEquipment/MyEquipment.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom"
+import light from "../../assets/light-bulb1.jpeg"
+import plug from "../../assets/plug1.jpg"
+import solar from "../../assets/solar-panel.jpeg"
+
+function MyEquipment(props) {
+  return (
+    <>
+    <div className='buying'>
+      <h2>My Equipment</h2>
+      <div className='items'>
+        <img className='image' src={solar} alt='A Solar Panel'/>
+        <img className='image' src={plug} alt='An eco-friendly plug'/>
+        <img className='image' src={light} alt='An Edison light bulb' />
+      </div>
+        <Link className="my-equipment-link" to="/myequipment">  
+      <button className="seemore-button">
+            See All
+      </button>
+      </Link>
+    </div>
+    
+  </>
+  );
+}
+
+export default MyEquipment;
