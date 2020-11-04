bf5637ec949fc6a7845303673cdf1e737aa9a55f
Geoff <geoffreylaporte@gmail.com>
2020-11-04 17:46:00 -0600

    Sort component completed

---
+++ client/src/components/Sort/Sort.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Sort.css'
+
+const Sort = (props) => {
+
+    const handleChange = (event) => {
+        props.onChange(event.target.value)
+    }
+
+  return (
+    <div>
+      <form className="sort-container" onSubmit={props.handleSubmit}>
+            <label className='label-sort' htmlFor="sort"> Results: {props.queriedEquipments.length}</label>
+            <select className="sort" onChange={handleChange}>
+                <option className="option" value="name-ascending" >&nbsp; Alphabetically, A-Z &nbsp;</option>
+                <option value="name-descending">&nbsp; Alphabetically, Z-A &nbsp;</option>
+                <option value="price-ascending">&nbsp; Price, low to high &nbsp;</option>
+                <option value="price-descending">&nbsp; Price, high to low &nbsp;</option>
+            </select>
+      </form>
+      </div>
+    )
+}
+
+export default Sort
