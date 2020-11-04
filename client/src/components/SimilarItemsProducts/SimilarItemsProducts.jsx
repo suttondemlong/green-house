5fc6076cea2b9424e1f5d2a221c669c37a478781
Dean <dean.g.leong@gmail.com>
2020-11-04 12:26:33 -0500

    similar items component

---
+++ client/src/components/SimilarItemsProducts/SimilarItemsProducts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SimilarItemsProducts(props) {
+  return (
+    <div className='buying'>
+      <h2>Similar Items</h2>
+      <div className="items-similar">
+        <img></img>
+        <img></img>
+        <img></img>
+      </div>
+    </div>
+  );
+}
+
+export default SimilarItemsProducts;
