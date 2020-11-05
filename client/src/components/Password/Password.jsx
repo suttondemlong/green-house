64272f3e2a261f6f9ea825da4fabbfb4b99cb2a2
Geoff <geoffreylaporte@gmail.com>
2020-11-04 22:53:53 -0600

    Password input forms created

--- client/src/components/Password/Password.jsx
+++ client/src/components/Password/Password.jsx
@@ -4,6 +4,11 @@ function Password(props) {
   return (
     <div>
       <h2>CHANGE PASSWORD</h2>
+      
+      <h4>New Password</h4>
+      <h4>Confirm New Password</h4>
+      <input type='password'></input>
+      <input type='password'></input>
       <hr className="general-hr"></hr>
     </div>
   );
