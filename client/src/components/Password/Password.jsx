53fb08ea731d5bab0434e4a89e4c07b50db60dd3
Geoff <geoffreylaporte@gmail.com>
2020-11-04 21:56:31 -0600

    Updated media query for the savings components and added in a password and user verification component

---
+++ client/src/components/Password/Password.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Password(props) {
+  return (
+    <div>
+      <div>CHANGE PASSWORD</div>
+    </div>
+  );
+}
+
+export default Password;
--- client/src/screens/MyAccount/MyAccount.jsx
+++ client/src/screens/MyAccount/MyAccount.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import "./MyAccount.css"
 import Layout from '../../components/shared/Layout/Layout'
 import UserInformation from '../../components/UserInformation/UserInformation';
+import UserVerification from '../../components/UserVerification/UserVerification';
 function MyAccount(props) {
   return (
     <Layout>
       <div className="content-container">
         <h2 className="myaccount-title">My Account</h2>
         <UserInformation />
-      
+        <UserVerification />
       </div>
       </Layout>
   );
