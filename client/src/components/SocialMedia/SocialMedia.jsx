6dedbe3931502717a303ddf9d5eca1f586579bf7
Amber Moroe <ambermoorexc@gmail.com>
2020-11-09 02:11:39 -0600

    Hacky way to keep the toggle from moving the page in userpage

--- client/src/components/SocialMedia/SocialMedia.jsx
+++ client/src/components/SocialMedia/SocialMedia.jsx
@@ -15,7 +15,7 @@ function SocialMedia(props) {
   }
 
   return (
-    <div>
+    <div className="socialmedia-container">
       <h2 className='social-title'>SOCIAL MEDIA</h2>
       <div className='socialmedia-icons'>
       <img className='instagram' src={instagram} alt='instagram-logo'/>
@@ -23,7 +23,7 @@ function SocialMedia(props) {
         </div>
       <div className='social-add'>add account</div>
       <button className='social-button' onClick={handleClick}>Delete My Account</button>
-      {(toggle === false) ? <div className="toggle-div"></div> : <h3>We're sad to see you leave!</h3>}
+      {<h3 className="social-h3" style={{ color: (toggle===false) ? "white" : "black" }}>We're sad to see you leave!</h3>}
     </div>
   );
 }
